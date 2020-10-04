de1fe92b726fa50849517f02233f86ee62f2435b
prasys <pradeesh@prasys.info>
2020-10-05 09:52:27 +1300

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import pandas as pd
 from google.api_core.client_options import ClientOptions
 #from google.cloud import automl
 from google.cloud import automl_v1
@@ -10,10 +11,20 @@ def inline_text_payload(file_path):
     content = ff.read()
   return {'text_snippet': {'content': content, 'mime_type': 'text/plain'} }
 
+def read_pandas_csv(file_path,column_name=None):
+    df = pd.read_csv(file_path)
+    items = df[column_name].values.tolist()
+    for item in items:
+      
+
+
+    
+
+
 def pdf_payload(file_path):
   return {'document': {'input_config': {'gcs_source': {'input_uris': [file_path] } } } }
 
-def get_prediction(file_path, model_name):
+def get_prediction(file_path, model_name,column_name=None):
   options = ClientOptions(api_endpoint='automl.googleapis.com')
   prediction_client = automl_v1.PredictionServiceClient(client_options=options)
 
@@ -28,5 +39,14 @@ def get_prediction(file_path, model_name):
 if __name__ == '__main__':
   file_path = sys.argv[1]
   model_name = sys.argv[2]
+  try:
+    column_name = sys.argv[3]
+  except IndexError:
+    column_name = 'null'
 
-  prediction = (get_prediction(file_path, model_name))
+
+  if (column_name != 'null'):
+    prediction = (get_prediction(file_path, model_name,column_name))
+
+  else:
+    prediction = (get_prediction(file_path, model_name))
